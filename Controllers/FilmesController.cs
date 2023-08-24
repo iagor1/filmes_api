41ccb989fbfacec1dfe93298dbdcecef34c00e73
iagor ml <iagorluchesi@gmail.com>
2023-08-24 12:25:04 -0400

    changing to IActionResult

--- Controllers/FilmesController.cs
+++ Controllers/FilmesController.cs
@@ -10,12 +10,13 @@ public class FilmesController : ControllerBase
     private static List<Filme> filmes = new List<Filme>(); // criou uma lista da classe Filme
     private static int id=0;
     [HttpPost]
-    public void AddFilme([FromBody]Filme filme) // recebe o parâmetro filme do tipo Filme(classe)
+    public IActionResult AddFilme([FromBody]Filme filme) // recebe o parâmetro filme do tipo Filme(classe)
     //o from body serve pro swagger, significa q vamos fzr input de dados lá
     {
         filme.Id = id++;
         filmes.Add(filme); // add o filme na lista, o método .Add() é um método de List(lista genérica)
-    }
+        return CreatedAtAction(nameof(GetByIdFilmes), new {id = filme.Id}, filme); //o Created retorna o objeto que criamos no metodo GetById, o new id
+    }// é preciso pois no metodo do get temos o parâmetro ID e o ultimo parametro é o objeto que foi criado
 
     [HttpGet]
     public IEnumerable<Filme> GetFilmes([FromQuery]int skip = 0, int take = 20) // skip e take funcionam para paginação, o FromQuerry vai vir
@@ -24,13 +25,17 @@ public class FilmesController : ControllerBase
     }
     
     [HttpGet("{id}")]
-    public Filme? GetByIdFilmes(int id)
+    public IActionResult GetByIdFilmes(int id)
     {
-        return filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok();
     }
     [HttpGet("titulos/{name}")]
-    public Filme? GetByNames(string name)
+    public IActionResult GetByNames(string name)
     {
-        return filmes.Find(filme => filme.Titulo == name);
+        var filme = filmes.Find(filme => filme.Titulo == name);
+        if (filme == null) return NotFound();
+        return Ok();
     }
 }
