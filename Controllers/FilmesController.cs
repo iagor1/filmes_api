433dfacf29983215482eb363c4e3cd7d71d674ae
iagor molino luchesi <iagorluchesi@gmail.com>
2023-09-06 13:34:34 -0400

    added url pattern

--- Controllers/FilmesController.cs
+++ Controllers/FilmesController.cs
@@ -26,7 +26,7 @@ public class FilmesController : ControllerBase
     /// </summary>
     /// <returns>IActionResult</returns>
     /// <response code="201">If post works</response>
-    [HttpPost]
+    [HttpPost("CreateFilm")]
     [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult AddFilme([FromBody] CreateFilmeDto filmedto) // recebe o parâmetro filme do tipo Filme(classe)
     //o from body serve pro swagger, significa q vamos fzr input de dados lá
@@ -43,7 +43,7 @@ public class FilmesController : ControllerBase
     /// <returns>IActionResult</returns>
     /// <response code="200">If get works</response>
     /// <response code="400">If get dont work</response>
-    [HttpGet]
+    [HttpGet("GetFilmsPagination")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult GetFilmes([FromQuery] int skip = 0, int take = 20) // skip e take funcionam para paginação, o FromQuerry vai vir
@@ -65,7 +65,7 @@ public class FilmesController : ControllerBase
     /// <returns>IActionResult</returns>
     /// <response code="200">If get works</response>
     /// <response code="404">If get dont work</response>
-    [HttpGet("{id}")]
+    [HttpGet("GetFilmById/{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult GetByIdFilmes(int id)
@@ -82,7 +82,7 @@ public class FilmesController : ControllerBase
     /// <returns>IActionResult</returns>
     /// <response code="200">If get works</response>
     /// <response code="404">If get dont work</response>
-    [HttpGet("titulos_names/{name}")]
+    [HttpGet("GetFilmByName/{name}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult GetByTitulo(string name)
@@ -98,7 +98,7 @@ public class FilmesController : ControllerBase
     /// <returns>IActionResult</returns>
     /// <response code="204">If put works</response>
     /// <response code="404">If trying to edit some object that dont exist in database</response>
-    [HttpPut("{id}")]
+    [HttpPut("EditFilm/{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult UpdateFilme(int id, [FromBody] UpdateFilmeDto updatefilmeDto)
@@ -116,7 +116,7 @@ public class FilmesController : ControllerBase
     /// <returns>IActionResult</returns>
     /// <response code="204">If patch works</response>
     /// <response code="404">If trying to edit some object that dont exist in database</response>
-    [HttpPatch("{id}")]
+    [HttpPatch("EditParcialFilm/{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult EditarFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
@@ -141,7 +141,7 @@ public class FilmesController : ControllerBase
     /// <returns>IActionResult</returns>
     /// <response code="204">If deletion works</response>
     /// <response code="404">If trying to delete some object that dont exist in database</response>
-    [HttpDelete("{id}")]
+    [HttpDelete("DeleteById/{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
 
